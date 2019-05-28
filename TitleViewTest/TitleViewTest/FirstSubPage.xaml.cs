9bbb081c678eb4848a138bb0bdeed6bf1f47ff0e
Rafael Veronezi <ravero@outlook.com>
2019-05-28 20:29:24 -0300

    Código de exemplo da barra de título personalizada no Xamarin.Forms.

---
+++ TitleViewTest/TitleViewTest/FirstSubPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TitleViewTest
+{
+    public partial class FirstSubPage : ContentPage {
+        public FirstSubPage() {
+            InitializeComponent();
+
+            var titleView = new Image { Source = ImageSource.FromFile("caeno_logo") };
+            NavigationPage.SetTitleView(this, titleView);
+        }
+    }
+}
